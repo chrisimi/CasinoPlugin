0cceef68a885177f3877d1ee7353198f8af11960
chrisimi <mittermaier.c@a1.net>
2020-07-15 18:38:46 +0200

    created small class for common item stack operations ItemAPI.java

---
+++ src/com/chrisimi/casinoplugin/utils/ItemAPI.java
@@ -0,0 +1,37 @@
+package com.chrisimi.casinoplugin.utils;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class ItemAPI {
+
+    /**
+     * create a
+     * @param name
+     * @param material
+     * @return
+     */
+    public static ItemStack createItem(String name, Material material)
+    {
+        ItemStack itemStack = new ItemStack(material);
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setDisplayName(name);
+        itemStack.setItemMeta(meta);
+
+        return itemStack;
+    }
+
+    /**
+     * change the display name of the item stack
+     * @param itemStack instance of the item stack
+     * @param newName the new name of the item stack
+     */
+    public static void changeName(ItemStack itemStack, String newName)
+    {
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setDisplayName(newName);
+
+        itemStack.setItemMeta(meta);
+    }
+}
