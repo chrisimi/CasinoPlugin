5f17c4ae69a616098ac64fc8111ebea285947d69
chrisimi <mittermaier.c@a1.net>
2020-10-14 19:41:58 +0200

    added EditHologramCommand for /casino edithologram [name]

---
+++ src/com/chrisimi/casinoplugin/commands/holograms/EditHologramCommand.java
@@ -0,0 +1,57 @@
+package com.chrisimi.casinoplugin.commands.holograms;
+
+import com.chrisimi.casinoplugin.hologramsystem.HologramMenu;
+import com.chrisimi.casinoplugin.hologramsystem.HologramSystem;
+import com.chrisimi.casinoplugin.hologramsystem.LBHologram;
+import com.chrisimi.casinoplugin.main.Main;
+import com.chrisimi.casinoplugin.main.MessageManager;
+import com.chrisimi.casinoplugin.scripts.CasinoManager;
+import com.chrisimi.commands.Command;
+import com.chrisimi.commands.Event;
+import com.chrisimi.commands.PermissionType;
+import org.bukkit.entity.Player;
+
+/**
+ * command instance for /casino edithologram [name]
+ */
+public class EditHologramCommand extends Command
+{
+    public EditHologramCommand()
+    {
+        this.command = "edithologram";
+        this.description = "edit hologram [name]";
+        this.permissions = new String[] {"casino.admin", "casino.hologram.server", "casino.hologram.create"};
+        this.permissionType = PermissionType.OR;
+        this.enableArguments = true;
+    }
+
+    @Override
+    public void execute(Event event)
+    {
+        if(event.getArgs().length == 0)
+            return;
+
+        Player player = event.getPlayer();
+
+        LBHologram holo = HologramSystem.getHologramByName(event.getArgs()[0]);
+        if(holo == null)
+        {
+            event.getPlayer().sendMessage(CasinoManager.getPrefix() + MessageManager.get("commands_invalid_hologramname"));
+            return;
+        }
+
+        if (holo.isServerHologram() && !(Main.perm.has(player, "casino.admin") || Main.perm.has(player, "casino.hologram.server")))
+        {
+            player.sendMessage(CasinoManager.getPrefix() + MessageManager.get("commands-player_no_permission"));
+            return;
+        }
+
+        if (!holo.isServerHologram() && !(holo.getOwner().getUniqueId().equals(player.getUniqueId())) && !(Main.perm.has(player, "casino.admin")))
+        {
+            player.sendMessage(CasinoManager.getPrefix() + MessageManager.get("commands-player_no_permission"));
+            return;
+        }
+
+        new HologramMenu(player, holo);
+    }
+}
