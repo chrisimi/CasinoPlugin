990d197235c9b97c80d6d472712e512f68a656a9
chrisimi <mittermaier.c@a1.net>
2020-10-16 19:48:02 +0200

    added DeleteResetCommand for command /casino deletereset

---
+++ src/com/chrisimi/casinoplugin/commands/DeleteResetCommand.java
@@ -0,0 +1,56 @@
+package com.chrisimi.casinoplugin.commands;
+
+import com.chrisimi.casinoplugin.main.Main;
+import com.chrisimi.casinoplugin.main.MessageManager;
+import com.chrisimi.casinoplugin.scripts.CasinoManager;
+import com.chrisimi.casinoplugin.scripts.LeaderboardsignsManager;
+import com.chrisimi.casinoplugin.serializables.Leaderboardsign;
+import com.chrisimi.casinoplugin.utils.CommandUtils;
+import com.chrisimi.commands.Command;
+import com.chrisimi.commands.Event;
+import com.chrisimi.commands.PermissionType;
+import com.chrisimi.commands.UsageType;
+import org.bukkit.entity.Player;
+
+/**
+ * the command instance for command /casino deletereset
+ */
+public class DeleteResetCommand extends Command
+{
+    public DeleteResetCommand()
+    {
+        this.command = "deletereset";
+        this.description = "delete the manual reset form a sign you are looking onto it";
+        this.permissions = new String[] {"casino.admin", "casino.serversigns", "casino.dice.create", "casino.slots.create", "casino.blackjack.create"};
+        this.permissionType = PermissionType.OR;
+        this.usageType = UsageType.PLAYER;
+    }
+
+    @Override
+    public void execute(Event event)
+    {
+        Player player = event.getPlayer();
+        Leaderboardsign sign = CommandUtils.getLeaderboardSignFromLookingOntoIt(event.getPlayer());
+        if(sign == null)
+            return;
+
+        //check for enough permission
+        if (sign.isServerSign() && (!Main.perm.has(player, "casino.admin") || !Main.perm.has(player, "casino.serversigns")))
+        {
+            player.sendMessage(CasinoManager.getPrefix() + MessageManager.get("commands-player_no_permission"));
+            return;
+        }
+
+        if (!sign.isServerSign() && !sign.getPlayer().getUniqueId().equals(player.getUniqueId()))
+        {
+            player.sendMessage(CasinoManager.getPrefix() + MessageManager.get("commands-player_no_permission"));
+            return;
+        }
+
+        //change values
+        sign.lastManualReset = 0;
+        sign.validUntil = 0;
+        LeaderboardsignsManager.save();
+        player.sendMessage(CasinoManager.getPrefix() + MessageManager.get("commands-leaderboard_delete_successful"));
+    }
+}
--- src/com/chrisimi/casinoplugin/utils/CommandUtils.java
+++ src/com/chrisimi/casinoplugin/utils/CommandUtils.java
@@ -3,7 +3,9 @@ package com.chrisimi.casinoplugin.utils;
 import com.chrisimi.casinoplugin.main.Main;
 import com.chrisimi.casinoplugin.main.MessageManager;
 import com.chrisimi.casinoplugin.scripts.CasinoManager;
+import com.chrisimi.casinoplugin.scripts.LeaderboardsignsManager;
 import com.chrisimi.casinoplugin.scripts.PlayerSignsManager;
+import com.chrisimi.casinoplugin.serializables.Leaderboardsign;
 import com.chrisimi.casinoplugin.serializables.PlayerSignsConfiguration;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
@@ -12,7 +14,7 @@ import org.bukkit.entity.Player;
 public class CommandUtils
 {
     /**
-     * check if the player is looking onto a sign
+     * check if the player is looking onto a player sign
      * @param player the player to use for it
      * @return {@link PlayerSignsConfiguration} if it is a valid player sign
      */
@@ -46,5 +48,34 @@ public class CommandUtils
         }
 
         return cnf;
+    }
+
+    /**
+     * check if the player is looking onto a leaderboard sign
+     * @param player the player to use for it
+     * @return {@link Leaderboardsign} if it is a valid leaderboard sign
+     */
+    public static Leaderboardsign getLeaderboardSignFromLookingOntoIt(Player player)
+    {
+        //get the block the player is currently looking at
+        Block block = player.getTargetBlock(null, 10);
+        if(block == null)
+            return null;
+
+        //check if it's a sign
+        if(!(block.getType().toString().contains("SIGN")))
+        {
+            player.sendMessage(CasinoManager.getPrefix() + MessageManager.get("commands-player-playersigns_not_a_sign"));
+            return null;
+        }
+
+        //check if the sign is a leaderboard sign
+        Leaderboardsign leaderboardsign = LeaderboardsignsManager.getLeaderboardsign(block.getLocation());
+        if (leaderboardsign == null)
+        {
+            player.sendMessage(CasinoManager.getPrefix() + MessageManager.get("commands-leaderboard_not_a_lbsign"));
+            return null;
+        }
+        return leaderboardsign;
     }
 }
