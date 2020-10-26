c37d5d0af3d677204d3fb42639614415a80c901f
chrisimi <mittermaier.c@a1.net>
2020-10-26 15:25:28 +0100

    added JackpotManager and jackpot.json file management

---
+++ src/com/chrisimi/casinoplugin/scripts/JackpotManager.java
@@ -0,0 +1,19 @@
+package com.chrisimi.casinoplugin.scripts;
+
+public class JackpotManager
+{
+    public JackpotManager()
+    {
+
+    }
+
+    private void init()
+    {
+
+    }
+
+    private synchronized void importJackpots()
+    {
+
+    }
+}
