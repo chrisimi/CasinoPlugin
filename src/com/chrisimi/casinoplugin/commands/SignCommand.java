91b37c07b5b6f86cc4ec86a6c27e6e3fb04fdb61
chrisimi <mittermaier.c@a1.net>
2020-10-30 19:05:37 +0100

    implemented sign command for dice signs

--- src/com/chrisimi/casinoplugin/commands/SignCommand.java
+++ src/com/chrisimi/casinoplugin/commands/SignCommand.java
@@ -1,8 +1,14 @@
 package com.chrisimi.casinoplugin.commands;
 
+import com.chrisimi.casinoplugin.main.MessageManager;
+import com.chrisimi.casinoplugin.scripts.CasinoManager;
+import com.chrisimi.casinoplugin.serializables.PlayerSignsConfiguration;
+import com.chrisimi.casinoplugin.utils.CommandUtils;
 import com.chrisimi.commands.Command;
 import com.chrisimi.commands.Event;
 import com.chrisimi.commands.PermissionType;
+import com.chrisimi.numberformatter.NumberFormatter;
+import org.bukkit.entity.Player;
 
 /**
  * the command instance for the command /casino sign
@@ -21,6 +27,32 @@ public class SignCommand extends Command
     @Override
     public void execute(Event event)
     {
-        //TODO
+        PlayerSignsConfiguration psc = CommandUtils.getPlayerSignFromLookingOntoIt(event.getPlayer());
+        if(psc == null)
+        {
+            event.getPlayer().sendMessage(CasinoManager.getPrefix() + MessageManager.get("commands-sign-no_sign"));
+            return;
+        }
+
+        event.getPlayer().sendMessage(CasinoManager.getPrefix() + String.format("Information about sign at X: %s, Y: %s, Z: " + psc.getLocation().getBlockX(),
+                psc.getLocation().getBlockY(), psc.getLocation().getBlockZ()));
+        event.getPlayer().sendMessage(CasinoManager.getPrefix() + "§6type: " + psc.gamemode.toString());
+        event.getPlayer().sendMessage(CasinoManager.getPrefix() + ((psc.isSignDisabled()) ? "§4disabled" : "§aenabled"));
+
+        switch(psc.gamemode)
+        {
+            case DICE:
+                showDiceInformation(psc, event.getPlayer());
+        }
+    }
+
+    private void showDiceInformation(PlayerSignsConfiguration psc, Player player)
+    {
+        double winChance = (double)(Math.round((psc.getWinChancesDice()[1] - psc.getWinChancesDice()[0] + 1) * 100.0)) / 100.0;
+
+        player.sendMessage(CasinoManager.getPrefix() + "§6bet: §e" + NumberFormatter.format(psc.bet, false));
+        player.sendMessage(CasinoManager.getPrefix() + "possible win: " + NumberFormatter.format(psc.bet * psc.winMultiplicatorDice(), false));
+        player.sendMessage(CasinoManager.getPrefix() + "win-range between " + psc.getWinChancesDice()[0] + " and " + psc.getWinChancesDice()[1]);
+        player.sendMessage(CasinoManager.getPrefix() + "win chance: " + winChance + " %");
     }
 }
