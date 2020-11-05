9673db150a2b02b8335d89df7fdc0adb69979b27
chrisimi <mittermaier.c@a1.net>
2020-11-05 19:39:02 +0100

    found color mistake

--- src/com/chrisimi/casinoplugin/menues/BlackjackCreationMenu.java
+++ src/com/chrisimi/casinoplugin/menues/BlackjackCreationMenu.java
@@ -252,7 +252,7 @@ public class BlackjackCreationMenu extends Inventory implements IInventoryAPI
 
         if(winMultiplicand1 == Double.MIN_VALUE && winMultiplicand2.length == 0)
         {
-            lore.add("-§a win multiplicand not set");
+            lore.add("-§4 win multiplicand not set");
             valuesValid = false;
         } else
             lore.add("-§a win multiplicand set to " + ((winMultiplicand1 == Double.MIN_VALUE) ? String.format("%s to %s", winMultiplicand2[0], winMultiplicand2[1]) : winMultiplicand1));
