1b99ca86c47d9553e2131680a1dcf7361e90b3fb
chrisimi <mittermaier.c@a1.net>
2020-11-09 18:06:37 +0100

    added all needed database methods

--- src/com/chrisimi/casinoplugin/database/MySQLDataBase.java
+++ src/com/chrisimi/casinoplugin/database/MySQLDataBase.java
@@ -1,9 +1,59 @@
 package com.chrisimi.casinoplugin.database;
 
+import com.chrisimi.casinoplugin.serializables.PlayData;
+import com.chrisimi.casinoplugin.serializables.PlayerSignsConfiguration;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+import java.util.Calendar;
+import java.util.List;
+
 public class MySQLDataBase implements IDataBase
 {
     @Override
     public void init()
+    {
+
+    }
+
+    @Override
+    public boolean isOnline()
+    {
+        return false;
+    }
+
+    @Override
+    public List<PlayData> getPlayData(OfflinePlayer player)
+    {
+        return null;
+    }
+
+    @Override
+    public List<PlayData> getPlayData(OfflinePlayer player, Calendar fromDate, Calendar toDate)
+    {
+        return null;
+    }
+
+    @Override
+    public List<PlayData> getPlayData(OfflinePlayer player, long fromMillis, long toMillis)
+    {
+        return null;
+    }
+
+    @Override
+    public List<PlayData> getPlayData(Calendar fromDate, Calendar toDate)
+    {
+        return null;
+    }
+
+    @Override
+    public List<PlayData> getPlayData(long fromMillis, long toMillis)
+    {
+        return null;
+    }
+
+    @Override
+    public void addData(Player player, PlayerSignsConfiguration psc, double playAmount, double winAmount)
     {
 
     }
